ddf06f6f759793a226a8e623b1f23317353ac590
user <user@userui-MacBookPro.local>
2023-07-21 18:00:56 +0900

    add ledgerSigner

--- src/NcpRatio.js
+++ src/NcpRatio.js
@@ -2,6 +2,7 @@
 
 // import dotenv from 'dotenv';
 
+const { LedgerSigner } = require('@ethersproject/hardware-wallets');
 const ethers = require('ethers');
 require('dotenv').config();
 
@@ -12,26 +13,35 @@ async function NcpRatio () {
     // console.log(process.env.PRIVATE_KEY)
     // console.log(wallet)
     const abi = require('./NCPStaking.json');
+    const ledgerSigner = new LedgerSigner(provider);
+    console.log(ledgerSigner)
 
     // console.log(abi.abi)
-    const ncpStaking = new ethers.Contract('0x64d2ccd2C4c7aC869b9f776CbC7b4d6c6fdc6022', abi.abi, wallet);
-    // console.log(ncpStaking)
-    
+    const ncpStaking = new ethers.Contract('0x64d2ccd2C4c7aC869b9f776CbC7b4d6c6fdc6022', abi.abi, ledgerSigner);
+    // console.log('ncpStaking : ',ncpStaking)
     return ncpStaking;
 }
+NcpRatio();
+
+
+// 요청 먼저 하고,
+async function RequestRatio(){
+    const stakingNCP = await NcpRatio();
+    const RatioRequest = await stakingNCP.setRewardFeeRatioRequest(NCPID, _feeRatio, {
+        gasPrice : ethers.utils.parseUnits('101','gwei'),
+        gasLimit : "100000",
+    })
+    return RatioRequest;
+}
+
+// 7일 뒤에 확정 짓고.
 async function showRatio (){
     const stakingNCP =await NcpRatio();
     const showFee = await stakingNCP.setRewardFeeRatio(1, {
         gasPrice : ethers.utils.parseUnits('101','gwei'),
         gasLimit : "100000",
-    })
-    const getFee = await stakingNCP.getRewarder(2)
-    console.log(getFee)
-    // console.log(showFee)
+    })    
     return showFee
-}
-showRatio();
 
-// const commissions = await getNCPStaking.setRewardFeeRatio(10, {gasPrice : ethers.utils.parseUnits('101','gwei')});
-// console.log(commissions)
-// export default NcpRatio;
+}
+// showRatio();
--- src/ledgerSigned.js
+++ src/ledgerSigned.js
@@ -5,9 +5,10 @@ const { ethers } = require("ethers");
 async function ledgerSigned () { 
     const provider = new ethers.providers.JsonRpcProvider('https://api.test.wemix.com')
     console.log(provider)
-    // const ledgerSigner = new LedgerSigner(provider, "hid", "TESTNET") 
     const ledgerSigner = new LedgerSigner(provider);
     console.log(ledgerSigner)
+    const abi = require('./NCPStaking.json');
+    
+
 }
 ledgerSigned();
-
