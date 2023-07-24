e1b54628588bd658905c4b014dfdb94de8d2adaf
BCSungjin <asdf256617@gmail.com>
2023-07-24 23:14:50 +0900

    add tx.wait()

--- src/NcpRatio.js
+++ src/NcpRatio.js
@@ -2,10 +2,10 @@
 
 // import dotenv from 'dotenv';
 
-const { LedgerSigner } = require('@ethersproject/hardware-wallets');
-const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid')
-const ethers = require('ethers');
-require('dotenv').config();
+const { LedgerSigner } = require("@ethersproject/hardware-wallets");
+const TransportNodeHid = require("@ledgerhq/hw-transport-node-hid");
+const ethers = require("ethers");
+require("dotenv").config();
 
 // async function checkLedgerConnection() {
 //     try{
@@ -19,54 +19,50 @@ require('dotenv').config();
 // }
 // checkLedgerConnection().then(transport => console.log("연결"));
 
-async function NcpRatio () {
-    const provider = new ethers.providers.JsonRpcProvider('https://api.wemix.com')
+async function NcpRatio() {
+  const provider = new ethers.providers.JsonRpcProvider("https://api.wemix.com");
 
+  //chain Id Check
+  //   const getChainId = (await provider.getNetwork()).chainId;
+  //   console.log(getChainId);
 
-    //chain Id Check 
-    // const getChainId = (await provider.getNetwork()).chainId
-    // console.log(getChainId)
-    
-    // const wallet = (new ethers.Wallet(process.env.PRIVATE_KEY)).connect(provider);
+  // const wallet = (new ethers.Wallet(process.env.PRIVATE_KEY)).connect(provider);
 
-    // console.log(process.env.PRIVATE_KEY)
-    // console.log(wallet)
-    const abi = require('./NCPStaking.json');
-    const ledgerSigner = new LedgerSigner(provider);
-    console.log(ledgerSigner)
+  // console.log(process.env.PRIVATE_KEY)
+  // console.log(wallet)
+  const abi = require("./NCPStaking.json");
+  const ledgerSigner = new LedgerSigner(provider);
+  //   console.log(ledgerSigner);
 
-    // console.log(abi.abi)
-    const ncpStaking = new ethers.Contract('0x088AcFcd91aEEB39fF9aDbC0f5b5c36749D89fea', abi.abi, ledgerSigner);
-    // console.log('ncpStaking : ',ncpStaking)
-    return ncpStaking;
+  // console.log(abi.abi)
+  const ncpStaking = new ethers.Contract("0x088AcFcd91aEEB39fF9aDbC0f5b5c36749D89fea", abi.abi, ledgerSigner);
+  // console.log('ncpStaking : ',ncpStaking)
+  return ncpStaking;
 }
 // NcpRatio();
 
-
 // 요청 먼저 하고,
-async function RequestRatio(){
-    const stakingNCP = await NcpRatio();
-    const NCPID = 15;
-    const feeRatio = 500;
-    const RatioRequest = await stakingNCP.setRewardFeeRatioRequest(NCPID, feeRatio, {
-        gasPrice : ethers.utils.parseUnits('101','gwei'),
-        gasLimit : "100000",
-    })
-    return RatioRequest;
+async function RequestRatio() {
+  const stakingNCP = await NcpRatio();
+  const RatioRequest = await stakingNCP.setRewardFeeRatioRequest(15, 500, {
+    gasPrice: ethers.utils.parseUnits("101", "gwei"),
+    gasLimit: "100000",
+  });
+  const transactionReceipt = await RatioRequest.wait();
+  console.log(transactionReceipt);
 }
 RequestRatio().catch((err) => {
-    console.log(err)
+  console.log(err);
 });
 
 // 7일 뒤에 확정 짓고.
-async function showRatio (){
-    const stakingNCP =await NcpRatio();
-    const showFee = await stakingNCP.setRewardFeeRatio(1, {
-        gasPrice : ethers.utils.parseUnits('101','gwei'),
-        gasLimit : "100000",
-        chainId: '1111',
-    })    
-    return showFee
-
+async function showRatio() {
+  const stakingNCP = await NcpRatio();
+  const showFee = await stakingNCP.setRewardFeeRatio(1, {
+    gasPrice: ethers.utils.parseUnits("101", "gwei"),
+    gasLimit: "100000",
+    chainId: "1111",
+  });
+  return showFee;
 }
 // showRatio();
---
+++ src/sendAccount.js
@@ -0,0 +1,7 @@
+const { ethers } = require("ethers");
+
+require("dotenv").config();
+
+async function sendAccount() {
+  const provider = new ethers.providers.JsonRpcProvider("https://api.wemix.com");
+}
